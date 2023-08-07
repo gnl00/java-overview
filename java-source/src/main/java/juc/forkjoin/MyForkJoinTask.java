30b6cdd6e2c6d32f1eb96544d31a4b1dfdca2f7f
liguangning <gnl0@qq.com>
2023-08-07 11:42:44 +0800

    update structure

---
+++ java-source/src/main/java/juc/forkjoin/MyForkJoinTask.java
@@ -0,0 +1,11 @@
+package juc.forkjoin;
+
+
+import java.util.concurrent.RecursiveTask;
+
+public class MyForkJoinTask extends RecursiveTask<Long> {
+    @Override
+    protected Long compute() {
+        return null;
+    }
+}
